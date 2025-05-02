f1494526bda1a6ad5204c0e00172bf55426c1916
Pankaj78172 <pankajpaliwal61@gmail.com>
2025-05-02 16:36:12 +0000

    gadget_review

--- main.js
+++ main.js
@@ -38,4 +38,15 @@ window.addEventListener('scroll', () => {
   });
 
 
+function filterReviews() {
+  const input = document.getElementById("searchInput").value.toLowerCase();
+  const cards = document.querySelectorAll(".review-card");
+
+  cards.forEach(card => {
+    const title = card.querySelector("h2").textContent.toLowerCase();
+    card.style.display = title.includes(input) ? "block" : "none";
+  });
+}
+
+
 
